0884bbffd2cba63a0fc8b58498016bf394413a99
Duy Le <lekhanhduyduy1201@gmail.com>
2024-06-30 00:12:58 +0700

    user service

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -7,21 +7,24 @@ import {
   Post,
 } from '@nestjs/common';
 import { UserDto } from './dto/user.dto';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+  constructor(private userService: UsersService) {}
+
   @Get()
   getAll() {
     return 'get all users';
   }
 
   @Post()
-  createUser(@Body() user: UserDto): Pick<UserDto, 'username'> {
+  createUser(@Body() user: UserDto): UserDto {
     user.id = Date.now().toString();
     user.createdAt = new Date();
     user.updatedAt = new Date();
 
-    return UserDto.plainToClass(user);
+    return this.userService.createUser(user);
   }
 
   @Get(':id')
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
+import { UserDto } from './dto/user.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  createUser(user: UserDto): UserDto {
+    return UserDto.plainToClass(user);
+  }
+}
