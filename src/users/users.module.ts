723f6cea17aec85ae341cef0ab8e4ecb8ea3e190
Duy Le <lekhanhduyduy1201@gmail.com>
2024-06-30 18:32:00 +0700

    useValue

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { StoreConfig } from 'src/store/store.config';
 
 @Module({
-  providers: [UsersService],
+  providers: [
+    UsersService,
+    {
+      provide: 'STORE_CONFIG',
+      useValue: {
+        dir: 'src',
+        path: 'user',
+      } as StoreConfig,
+    },
+  ],
   controllers: [UsersController],
 })
 export class UsersModule {}
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,8 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
 import { UserDto } from './dto/user.dto';
+import { StoreConfig } from 'src/store/store.config';
 
 @Injectable()
 export class UsersService {
+  constructor(@Inject('STORE_CONFIG') storeConfig: StoreConfig) {
+    console.log('log--storeConfig ', storeConfig);
+  }
   createUser(user: UserDto): UserDto {
     return UserDto.plainToClass(user);
   }
